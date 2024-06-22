b9f41d3164b374d7d0887fcc96b9a238cd358098
CamPo <166082757+LaHechicera@users.noreply.github.com>
2024-06-22 19:22:08 -0400

    Update GatoEstadistica.py
    Cambio final Proyecto gato

--- GatoEstadistica.py
+++ GatoEstadistica.py
@@ -1,5 +1,10 @@
 import statistics as st
 
+datos_numeros = ("10\n20\n20\n30\n40\n50\n50\n60\n")
+with open('datos.txt', 'w') as archivo:
+    archivo.writelines(datos_numeros)
+print(f"Datos de 'datos.txt'\n{datos_numeros}")
+
 try:
     with open('datos.txt', 'r') as datos:
         numeros = datos.read().splitlines()
@@ -8,8 +13,11 @@ try:
         mediana = st.median(numeros)
         promedio = sum(numeros) / len(numeros)
         promFinal = round(promedio,1)
+        moda = st.mode(numeros)
 
-        modas = st.mode(numeros)
+    print(f"Promedio: {promedio}")
+    print(f"Mediana: {mediana}")
+    print(f"Moda: {moda}")
 
 except:
     print("ERROR: ERROR EN EL PROGRAMA. CONSULTAR AL DUEÑO")
