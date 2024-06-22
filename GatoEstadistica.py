dbe30ddcbd213011e4f12557c236d0a6064c4c9d
LaHechicera <ca.gonzalezz@duocuc.cl>
2024-06-22 10:16:10 -0400

    Segunda parte del proyecto

--- GatoEstadistica.py
+++ GatoEstadistica.py
@@ -1,1 +1,8 @@
-"hola"
+        mediana = st.median(numeros)
+        promedio = sum(numeros) / len(numeros)
+        promFinal = round(promedio,1)
+
+        modas = st.mode(numeros)
+
+except:
+    print("ERROR: ERROR EN EL PROGRAMA. CONSULTAR AL DUEÑO")
