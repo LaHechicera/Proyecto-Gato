1081ed04b88a703f6cd499ada5524d6df3a65ed1
alvidi12 <an.villarroeld@duocuc.cl>
2024-06-22 10:15:58 -0400

    primera parte aiuda

--- GatoEstadistica.py
+++ GatoEstadistica.py
@@ -1,1 +1,7 @@
-"hola"
+import statistics as st
+
+try:
+    with open('datos.txt', 'r') as datos:
+        numeros = datos.read().splitlines()
+        intConvertor = [int (i) for i in numeros]   #Convertidor de string a integer
+        numeros = intConvertor
